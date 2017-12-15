c8ed829bdb3f3b77305d8876c50a106d28388a5f
sebam2k4 <sebam2k4@gmail.com>
2017-12-15 12:09:00 +0000

    generated questionList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { QuestionListComponent } from './components/question-list/question-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    QuestionListComponent
   ],
   imports: [
     BrowserModule
