58e078f2567663bcc81a4c2cf5cc6b745eff15b2
sebam2k4 <sebam2k4@gmail.com>
2017-12-15 12:23:50 +0000

    created questions object

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -6,8 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-list.component.css']
 })
 export class QuestionListComponent implements OnInit {
+  questions: Object[];
 
-  constructor() { }
+  constructor() {
+    this.questions = [
+      {
+        text: 'What is your name?',
+        answer: 'My name is Sebastian'
+      },
+      {
+        text: 'What is your favourite color?',
+        answer: 'My favourite color is blue'
+      },
+      {
+        text: 'What is your favourite Coding language?',
+        answer: 'Javascript'
+      }
+    ];
+  }
 
   ngOnInit() {
   }
