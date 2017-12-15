aa4653abe110d165499130169571861b0d4279af
sebam2k4 <sebam2k4@gmail.com>
2017-12-15 21:16:11 +0000

    Added local storage for questions

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,6 +7,7 @@ export class DataService {
   questions:Question[];
 
   constructor() {
+    /* will be using local storage instead
     this.questions = [
       {
         text: 'What is your name?',
@@ -23,22 +24,53 @@ export class DataService {
         answer: 'Javascript',
         hide: true
       }
-    ];
+    ]; */
    }
 
+   // get questions from local storage
    getQuestions() {
-     return this.questions;
+    // Local Storage - needs to be saved in string (JSON.stringify) and 
+    // when pulling out data need to convert back to an array (JSON.parse)
+    if(localStorage.getItem('questions') === null){
+      this.questions = [];
+    } else {
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
+    
+
+    return this.questions;
    }
 
+   // add question from local storage
    addQuestion(question:Question) {
     this.questions.unshift(question);
+
+    //init local variable
+    let questions;
+
+    if(localStorage.getItem('questions') === null){
+      questions = [];
+      // Push new question
+      questions.unshift(question);
+      // Set new array to local storage
+      localStorage.setItem('questions', JSON.stringify(questions));
+    } else {
+      questions = JSON.parse(localStorage.getItem('questions'));
+      // add new question
+      questions.unshift(question);
+      // reset local storage
+      localStorage.setItem('questions', JSON.stringify(questions));
+    }
    }
 
+   // remove question from local storage
    removeQuestion(question:Question) {
-     //match question with button
-     for(let i = 0; i < this.questions.length; i++){
+    //match question with button to delete correct question
+    for(let i = 0; i < this.questions.length; i++){
       if(question == this.questions[i])
         this.questions.splice(i,1);
+        // reset local storage
+        localStorage.setItem('questions', JSON.stringify(this.questions));
      }
    }
 }
