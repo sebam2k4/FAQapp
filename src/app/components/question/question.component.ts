7fce3f1601cd8daf6cdb2185a010802d98a6ee7b
sebam2k4 <sebam2k4@gmail.com>
2017-12-15 13:44:53 +0000

    Nested question component inside questions-list

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Question } from '../../models/Question';
 
 @Component({
   selector: 'app-question',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent implements OnInit {
+  @Input('question') question:Question;
 
   constructor() { }
 
